a1813e5fc18b7c39ac2854087636aa05b607e304
Matthew McCaskill <mattrmccaskill@gmail.com>
2024-02-15 19:44:13 -0600

    attempt vercel fix part 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "dist",
+  },
   plugins: [react(), TanStackRouterVite()],
 });
