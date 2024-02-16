1628fea7d01732e507a0e8a00c54a79e48c83ea8
Matthew McCaskill <mattrmccaskill@gmail.com>
2024-02-15 19:52:25 -0600

    found the fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "dist",
-  },
   plugins: [react(), TanStackRouterVite()],
 });
