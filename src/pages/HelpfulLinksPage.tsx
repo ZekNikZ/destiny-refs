c15cc81cbb9bedd7686ff04933c5676c25e6e6f9
Matthew McCaskill <mattrmccaskill@gmail.com>
2025-03-02 02:45:47 -0600

    Update styling of links page

--- src/pages/HelpfulLinksPage.tsx
+++ src/pages/HelpfulLinksPage.tsx
@@ -1,15 +1,22 @@
-import { Title, Card, Group, Stack } from "@mantine/core";
+import { Title, Card, Group, Stack, Box } from "@mantine/core";
 import { Link } from "react-router-dom";
 import {
+  Axe,
   Backpack,
   ChartBar,
   FlagCheckered,
+  Gavel,
   Info,
   Intersect,
   ListChecks,
+  Medal,
   Newspaper,
   Ranking,
+  Rewind,
+  Sword,
   Trash,
+  Users,
+  UsersFour,
   type Icon,
 } from "@phosphor-icons/react";
 
@@ -26,10 +33,10 @@ function makePhosphorIcon(Icon: Icon) {
 function LinkTile(props: LinkData) {
   return (
     <Link to={props.link} style={{ textDecoration: "none" }} target="_blank">
-      <Card shadow="sm" padding="xs" radius="sm" withBorder w={200} h={200}>
+      <Card shadow="sm" padding="xs" radius="sm" withBorder w={180} h={180}>
         <Stack align="center" justify="center" gap="xs" h="100%">
           {props.icon}
-          <Title size="h3" c="#fff" fw="bold" lh="xs" ta="center">
+          <Title size="h4" c="#fff" fw="bold" lh="xs" ta="center">
             {props.title}
           </Title>
         </Stack>
@@ -40,52 +47,116 @@ function LinkTile(props: LinkData) {
 
 export default function HelpfulLinksPage() {
   return (
-    <Group gap="sm" wrap="wrap">
-      <LinkTile
-        title="Data Compendium"
-        link="https://docs.google.com/spreadsheets/d/1WaxvbLx7UoSZaBqdFr1u32F2uWVLo-CJunJB4nlGUE4/edit?gid=1038486120#gid=1038486120"
-        icon={makePhosphorIcon(Info)}
-      />
-      <LinkTile
-        title="Boss Damage Spreadsheet"
-        link="https://docs.google.com/spreadsheets/d/1_5wtBjRYHHxuF4oJKDb_iOGZs-wTkzB6RYbnyNLbuz4/edit?gid=1313551887#gid=1313551887"
-        icon={makePhosphorIcon(ChartBar)}
-      />
-      <LinkTile
-        title="Endgame Tierlist Spreadsheet"
-        link="https://docs.google.com/spreadsheets/d/1JM-0SlxVDAi-C6rGVlLxa-J1WGewEeL8Qvq4htWZHhY/edit?gid=867153635#gid=867153635"
-        icon={makePhosphorIcon(Ranking)}
-      />
-      <LinkTile
-        title="Buff/Debuff Stacking Spreadsheet"
-        link="https://docs.google.com/spreadsheets/d/1i1KUwgVkd8qhwYj481gkV9sZNJQCE-C3Q-dpQutPCi4/edit?gid=242217075#gid=242217075"
-        icon={makePhosphorIcon(Intersect)}
-      />
-      <LinkTile
-        title="D2Checkpoint"
-        link="https://d2checkpoint.com/"
-        icon={makePhosphorIcon(FlagCheckered)}
-      />
-      <LinkTile
-        title="Destiny Item Manager"
-        link="https://app.destinyitemmanager.com"
-        icon={makePhosphorIcon(Backpack)}
-      />
-      <LinkTile
-        title="D2 Checklist"
-        link="https://www.d2checklist.com/home"
-        icon={makePhosphorIcon(ListChecks)}
-      />
-      <LinkTile
-        title="Bungie News"
-        link="https://www.bungie.net/7/en/News"
-        icon={makePhosphorIcon(Newspaper)}
-      />
-      <LinkTile
-        title="Time Wasted on Destiny"
-        link="https://wastedondestiny.com/"
-        icon={makePhosphorIcon(Trash)}
-      />
-    </Group>
+    <Box maw={756} m="auto">
+      <Stack>
+        <Title order={2} size="h2">
+          Spreadsheets
+        </Title>
+        <Group gap="sm" wrap="wrap">
+          <LinkTile
+            title="Data Compendium"
+            link="https://docs.google.com/spreadsheets/d/1WaxvbLx7UoSZaBqdFr1u32F2uWVLo-CJunJB4nlGUE4/edit?gid=1038486120#gid=1038486120"
+            icon={makePhosphorIcon(Info)}
+          />
+          <LinkTile
+            title="Boss Damage Spreadsheet"
+            link="https://docs.google.com/spreadsheets/d/1_5wtBjRYHHxuF4oJKDb_iOGZs-wTkzB6RYbnyNLbuz4/edit?gid=1313551887#gid=1313551887"
+            icon={makePhosphorIcon(ChartBar)}
+          />
+          <LinkTile
+            title="Endgame Tierlist Spreadsheet"
+            link="https://docs.google.com/spreadsheets/d/1JM-0SlxVDAi-C6rGVlLxa-J1WGewEeL8Qvq4htWZHhY/edit?gid=867153635#gid=867153635"
+            icon={makePhosphorIcon(Ranking)}
+          />
+          <LinkTile
+            title="Buff/Debuff Stacking Spreadsheet"
+            link="https://docs.google.com/spreadsheets/d/1i1KUwgVkd8qhwYj481gkV9sZNJQCE-C3Q-dpQutPCi4/edit?gid=242217075#gid=242217075"
+            icon={makePhosphorIcon(Intersect)}
+          />
+        </Group>
+        <Title order={2} size="h2">
+          Bungie
+        </Title>
+        <Group gap="sm" wrap="wrap">
+          <LinkTile
+            title="Bungie News"
+            link="https://www.bungie.net/7/en/News"
+            icon={makePhosphorIcon(Newspaper)}
+          />
+          <LinkTile
+            title="Previous Season Pass"
+            link="https://www.bungie.net/7/en/seasons/previousseason"
+            icon={makePhosphorIcon(Rewind)}
+          />
+        </Group>
+
+        <Title order={2} size="h2">
+          Utilities
+        </Title>
+        <Group gap="sm" wrap="wrap">
+          <LinkTile
+            title="D2Checkpoint"
+            link="https://d2checkpoint.com/"
+            icon={makePhosphorIcon(FlagCheckered)}
+          />
+          <LinkTile
+            title="Destiny Item Manager"
+            link="https://app.destinyitemmanager.com"
+            icon={makePhosphorIcon(Backpack)}
+          />
+          <LinkTile
+            title="Destiny Recipes Checklist"
+            link="https://destinyrecipes.com/checklist"
+            icon={makePhosphorIcon(ListChecks)}
+          />
+        </Group>
+
+        <Title order={2} size="h2">
+          Reports
+        </Title>
+        <Group gap="sm" wrap="wrap">
+          <LinkTile
+            title="Raid Report"
+            link="https://raid.report/"
+            icon={makePhosphorIcon(UsersFour)}
+          />
+          <LinkTile
+            title="Dungeon Report"
+            link="https://dungeon.report/"
+            icon={makePhosphorIcon(Users)}
+          />
+          <LinkTile
+            title="Raid Hub"
+            link="https://raidhub.io/"
+            icon={makePhosphorIcon(UsersFour)}
+          />
+          <LinkTile
+            title="Time Wasted on Destiny"
+            link="https://wastedondestiny.com/"
+            icon={makePhosphorIcon(Trash)}
+          />
+          <LinkTile
+            title="Trials Report"
+            link="https://destinytrialsreport.com/"
+            icon={makePhosphorIcon(Axe)}
+          />
+          <LinkTile
+            title="Crucible Report"
+            link="https://crucible.report/"
+            icon={makePhosphorIcon(Sword)}
+          />
+          <LinkTile
+            title="Grandmaster Report"
+            link="https://gm.report/"
+            icon={makePhosphorIcon(Gavel)}
+          />
+          <LinkTile
+            title="Emblems Report"
+            link="https://emblems.report/"
+            icon={makePhosphorIcon(Medal)}
+          />
+        </Group>
+      </Stack>
+    </Box>
   );
 }
