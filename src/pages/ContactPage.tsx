9e329c5278b976614593276c66a790f894d3e2e4
Matthew McCaskill <mattrmccaskill@gmail.com>
2025-03-06 22:00:53 -0600

    Better contact page

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,4 +1,4 @@
-import { Code, Container, Group, List, Stack, Text, Image } from "@mantine/core";
+import { Code, Container, Group, Stack, Text, Image, Divider } from "@mantine/core";
 import { DiscordLogo, GithubLogo } from "@phosphor-icons/react";
 
 export default function ContactPage() {
@@ -15,33 +15,25 @@ export default function ContactPage() {
         </Group>
         <Text>
           This website is created and maintained by ZekNikZ. You can find him at the following:
-          <List>
-            <List.Item>
-              <Group align="center" gap="xs">
-                <DiscordLogo weight="fill" /> Discord: <Code>@zeknikz</Code>
-              </Group>
-            </List.Item>
-            <List.Item>
-              <Group align="center" gap="xs">
-                <GithubLogo weight="fill" /> GitHub:
-                <a href="https://github.com/ZekNikZ">
-                  <Code>@ZekNikZ</Code>
-                </a>
-              </Group>
-            </List.Item>
-            <List.Item>
-              <Group align="center" gap="xs">
-                <Image
-                  src="/images/logos/destiny.svg"
-                  width={16}
-                  height={16}
-                  style={{ width: "16px" }}
-                />
-                Bungie:
-                <Code>ZekNikZ#3085</Code>
-              </Group>
-            </List.Item>
-          </List>
+          <Group align="center" gap="xs" h={30}>
+            <DiscordLogo weight="fill" size={20} /> Discord: <Code>@zeknikz</Code>
+          </Group>
+          <Group align="center" gap="xs" h={30}>
+            <GithubLogo weight="fill" size={20} /> GitHub:
+            <a href="https://github.com/ZekNikZ">
+              <Code>@ZekNikZ</Code>
+            </a>
+          </Group>
+          <Group align="center" gap="xs" h={30}>
+            <Image
+              src="/images/logos/destiny.svg"
+              width={20}
+              height={20}
+              style={{ width: "20px" }}
+            />
+            Bungie:
+            <Code>ZekNikZ#3085</Code>
+          </Group>
         </Text>
         <Text>
           If you find any issues (including incorrect loot tables or inaccurate information) or have
@@ -54,6 +46,38 @@ export default function ContactPage() {
           If you are a developer and would like to contribute, feel free to{" "}
           <a href="https://github.com/ZekNikZ/destiny-refs/pulls"> open a pull request</a>. I'll
           gladly accept the help!
+        </Text>
+        <Text>
+          If you would like to support this project, you can{" "}
+          <a href="https://ko-fi.com/zeknikz" target="_blank">
+            buy me a coffee
+          </a>
+          . This website and its content are and will always be free of charge, but the website
+          itself does cost money to operate. I greatly appreciate any and all support!
+        </Text>
+
+        <Divider my={15} />
+
+        <Text fs="italic">
+          This is a passion project of mine that has been over a year in the making. The goal of
+          this website is to be an accurate, regularly-updated, and easy to use resource for Destiny
+          players of all kinds. It came from a desire to have a single place that had up-to-date
+          information that could be easily shared and accessed quickly (both in terms of pulling it
+          up on a second monitor and actual site load speed). I've designed it in such a way to be
+          the exact resource I've wanted all these years, and I hope that you find it as useful as I
+          do! If not, I'm always open to suggestions and feedback. Enjoy!
+        </Text>
+        <Text fs="italic">- ZekNikZ</Text>
+
+        <Divider my={15} />
+
+        <Text fz={11} c="gray">
+          Destiny 2 is a registered trademark of Bungie, Inc. All content and assets used on this
+          website are the property of their respective owners. This website is not affiliated with,
+          endorsed by, or officially connected to Bungie in any way. All trademarks, logos, and
+          images are used for informational and fan purposes only under fair use. If you are a
+          rights holder and believe any content infringes on your intellectual property, please
+          contact us for prompt removal.
         </Text>
       </Stack>
     </Container>
